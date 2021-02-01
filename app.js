f01c8e5d3dd4ee3bd96e926b4f6a660432ca088c
youminji62 <youminji62@gmail.com>
2021-02-01 15:41:02 +0900

    코드 수정

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/',(req,res) => res.send('Hello, Nodejs!'));
+app.get('/',(req,res) => res.send('Hello, Nodejs! code update, heroku testing'));
 app.listen(port,()=>console.log('Example app listening on port 3000'))
